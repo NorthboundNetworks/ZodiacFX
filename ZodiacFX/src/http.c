26c60e7941fa60fb89f1970a16c39a1f83d94fdd
Kristopher Chen <kristopherchen.kc@gmail.com>
2016-11-24 13:56:45 +1100

    Pages moved into functions
    - Updated authorship
    - Moved html page creation into individual functions
    - Updated buffer overflow checking

--- ZodiacFX/src/http.c
+++ ZodiacFX/src/http.c
@@ -23,7 +23,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Author: Paul Zanna <paul@northboundnetworks.com>
+ * Authors: Paul Zanna <paul@northboundnetworks.com>
+ *		  & Kristopher Chen <Kristopher@northboundnetworks.com>
  *
  */
 
@@ -46,12 +47,20 @@ extern int32_t ul_temp;
 // Local Variables
 struct tcp_pcb *http_pcb;
 char http_buffer[512];		// Buffer for HTTP message storage
-char http_msg[64];
+char http_msg[64];			// Buffer for HTTP message filtering
 extern uint8_t shared_buffer[SHARED_BUFFER_LEN];
 
 static err_t http_recv(void *arg, struct tcp_pcb *pcb, struct pbuf *p, err_t err);
 static err_t http_accept(void *arg, struct tcp_pcb *pcb, err_t err);
 void http_send(char *buffer, struct tcp_pcb *pcb);
+
+uint8_t interfaceCreate_Frames(void);
+uint8_t interfaceCreate_Header(void);
+uint8_t interfaceCreate_Menu(void);
+uint8_t interfaceCreate_Home(void);
+uint8_t interfaceCreate_Config(void);
+uint8_t interfaceCreate_OpenFlow(void);
+uint8_t interfaceCreate_About(void);
 
 /*
 *	HTTP initialization function
@@ -88,9 +97,6 @@ static err_t http_recv(void *arg, struct tcp_pcb *pcb, struct pbuf *p, err_t err
 {
 	int len;
 	int i = 0;
-  	int hr = (totaltime/2)/3600;
-	int t = (totaltime/2)%3600;
-	int min = t/60;
 	char *pc;
 	memset(&http_msg, 0, sizeof(http_msg));	// Clear HTTP message array
 	
@@ -133,275 +139,89 @@ static err_t http_recv(void *arg, struct tcp_pcb *pcb, struct pbuf *p, err_t err
 				TRACE("http.c: resource request for %s", http_msg);
 			}
 			
-			// Check resource
+			// Check resource & serve page
 			if(http_msg[0] == '\0')
 			{
-				// Format HTTP response
-				sprintf(shared_buffer,"HTTP/1.1 200 OK\r\n");
-				strcat(shared_buffer,"Connection: close\r\n");
-				strcat(shared_buffer,"Content-Type: text/html; charset=UTF-8\r\n\r\n");
-				// Send frames
-				strcat(shared_buffer, \
-						"<!DOCTYPE html>"\
-						"<html>"\
-							"<head>"\
-								"<title>Zodiac FX</title>"\
-							"</head>"\
-							"<frameset rows=\"75,*\">"\
-								"<frame src=\"header.htm\" name=\"titleframe\" noresize scrolling=\"no\" frameborder=\"1\">"\
-								"<frameset cols=\"160, *\">"\
-									"<frameset rows=\"*,0\">"\
-									"<frame src=\"menu.htm\" name=\"sidebar\" noresize scrolling=\"no\" frameborder=\"1\">"\
-									"</frameset>"\
-									"<frame src=\"home.htm\" name=\"page\" scrolling=\"auto\" frameborder=\"1\">"\
-								"</frameset>"\
-								"<noframes>"\
-									"<body>"\
-									"<p>Browser version not supported."\
-									"</body>"\
-								"</noframes>"\
-							"</frameset>"\
-						"</html>"\
-							);
-				TRACE("http.c: html written to buffer");
-				http_send(&shared_buffer, pcb);
-				TRACE("http.c: HTTP sent successfully - %d bytes", strlen(shared_buffer));
-			}
-			else if(strcmp(http_msg,"header.htm") == 0)
-			{
-				// Send header
-				if( snprintf(shared_buffer, SHARED_BUFFER_LEN,\
-						"<!DOCTYPE html>"\
-						"<META http-equiv=\"refresh\" content=\"61\">"\
-						"<html>"\
-							"<head>"\
-							"<style>"\
-								"header {"\
-									"font-family:Sans-serif;"\
-									"position: absolute;"\
-									"top: 0;"\
-									"left: 0;"\
-									"width: 100%%;"\
-									"height: 100%%;"\
-									"overflow: hidden;"\
-									"color: white;"\
-									"background: black;"\
-								"}"\
-								"h1 {"\
-									"margin-top:20px;"\
-									"padding-left: 20px;"\
-								"}"\
-                				"p {"\
-									"font-family:Sans-serif;"\
-									"color: white;"\
-									"position: fixed;"\
-									"right: 150px;"\
-									"top: -5px;"\
-								"}"\
-							"</style>"\
-							"</head>"\
-							"<body>"\
-								"<header>"\
-									"<h1>Zodiac FX</h1>"\
-								"</header>"\
-                				"<p>"\
-									"Firmware Version: %s<br>"\
-									"CPU Temp: %d C<br>"\
-									"Uptime: %02d:%02d"\
-								"</p>"\
-							"</body>"\
-						"</html>"\
-							, VERSION, (int)ul_temp, hr, min) < SHARED_BUFFER_LEN)
+				if(interfaceCreate_Frames())
 				{
-					TRACE("http.c: html written to buffer");
 					http_send(&shared_buffer, pcb);
-					TRACE("http.c: HTTP sent successfully - %d bytes", strlen(shared_buffer));
+					TRACE("http.c: Page sent successfully - %d bytes", strlen(shared_buffer));
 				}
 				else
 				{
-					TRACE("http.c: WARNING: html truncated to prevent buffer overflow");
+					TRACE("http.c: Unable to serve page - buffer at %d bytes", strlen(shared_buffer));
+				}
+			}
+			else if(strcmp(http_msg,"header.htm") == 0)
+			{
+				if(interfaceCreate_Header())
+				{
+					http_send(&shared_buffer, pcb);
+					TRACE("http.c: Page sent successfully - %d bytes", strlen(shared_buffer));
+				}
+				else
+				{
+					TRACE("http.c: Unable to serve page - buffer at %d bytes", strlen(shared_buffer));
 				}
 			}
 			else if(strcmp(http_msg,"menu.htm") == 0)
 			{
-				// Send menu
-				if( snprintf(shared_buffer, SHARED_BUFFER_LEN,\
-					"<!DOCTYPE html>"\
-					"<html>"\
-					"<head>"\
-					"<style>"\
-					"body {"\
-						"font-family:Sans-serif;"\
-						"line-height: 1.9em;"\
-						"font-size: 20px;"\
-						"font-weight: bold;"\
-						"background: #F6F6F6;"\
-					"}"\
-					"body ul {"\
-						"list-style-type: none;"\
-						"margin: 10px;"\
-						"padding: 0;"\
-					"}"\
-					"body ul a {"\
-						"color: black;"\
-						"text-decoration: none;"\
-					"}"\
-					"body ul a:active {"\
-						"font-weight: 700;"\
-					"}"\
-					"</style>"\
-					"</head>"\
-					"<body>"\
-					"<ul>"\
-					"<li><a href=\"home.htm\" target=\"page\">Home</a></li>"\
-					"<li><a href=\"config.htm\" target=\"page\">Config</a></li>"\
-					"<li><a href=\"openflow.htm\" target=\"page\">OpenFlow</a></li>"\
-					"<li><a href=\"about.htm\" target=\"page\">About</a></li>"\
-					"</ul>"\
-					"</body>"\
-					"</html>"\
-							) < SHARED_BUFFER_LEN)
+				if(interfaceCreate_Menu())
 				{
-					TRACE("http.c: html written to buffer");
 					http_send(&shared_buffer, pcb);
-					TRACE("http.c: HTTP sent successfully - %d bytes", strlen(shared_buffer));
+					TRACE("http.c: Page sent successfully - %d bytes", strlen(shared_buffer));
 				}
 				else
 				{
-					TRACE("http.c: WARNING: html truncated to prevent buffer overflow");
+					TRACE("http.c: Unable to serve page - buffer at %d bytes", strlen(shared_buffer));
 				}
 			}
 			else if(strcmp(http_msg,"home.htm") == 0)
 			{
-				// Send body
-				if( snprintf(shared_buffer, SHARED_BUFFER_LEN,\
-					"<!DOCTYPE html>"\
-					"<html>"\
-						"<head>"\
-							"<style>"\
-								"body {"\
-									"overflow: auto;"\
-									"font-family:Sans-serif;"\
-									"font-size: 18px;"\
-									"margin-left: 20px;"\
-								"}"\
-							"</style>"\
-						"</head>"\
-						"<body>"\
-							"<p>"\
-							"Home Page<br>Placedholder text."\
-							"</p>"\
-						"</body>"\
-					"</html>"\
-							) < SHARED_BUFFER_LEN)
+				if(interfaceCreate_Home())
 				{
-					TRACE("http.c: html written to buffer");
 					http_send(&shared_buffer, pcb);
-					TRACE("http.c: HTTP sent successfully - %d bytes", strlen(shared_buffer));
+					TRACE("http.c: Page sent successfully - %d bytes", strlen(shared_buffer));
 				}
 				else
 				{
-					TRACE("http.c: WARNING: html truncated to prevent buffer overflow");
+					TRACE("http.c: Unable to serve page - buffer at %d bytes", strlen(shared_buffer));
 				}
 			}
       		else if(strcmp(http_msg,"config.htm") == 0)
 			{
-				// Send body
-				if( snprintf(shared_buffer, SHARED_BUFFER_LEN,""\
-					"<!DOCTYPE html>"\
-					"<html>"\
-						"<head>"\
-							"<style>"\
-								"body {"\
-									"overflow: auto;"\
-									"font-family:Sans-serif;"\
-									"font-size: 18px;"\
-									"margin-left: 20px;"\
-								"}"\
-							"</style>"\
-						"</head>"\
-						"<body>"\
-							"<p>"\
-							"Config Page<br>Placedholder text."\
-							"</p>"\
-						"</body>"\
-					"</html>"\
-							) < SHARED_BUFFER_LEN)
+				if(interfaceCreate_Config())
 				{
-					TRACE("http.c: html written to buffer");
 					http_send(&shared_buffer, pcb);
-					TRACE("http.c: HTTP sent successfully - %d bytes", strlen(shared_buffer));
+					TRACE("http.c: Page sent successfully - %d bytes", strlen(shared_buffer));
 				}
 				else
 				{
-					TRACE("http.c: WARNING: html truncated to prevent buffer overflow");
+					TRACE("http.c: Unable to serve page - buffer at %d bytes", strlen(shared_buffer));
 				}
 			}
       		else if(strcmp(http_msg,"openflow.htm") == 0)
 			{
-				// Send body
-				if( snprintf(shared_buffer, SHARED_BUFFER_LEN,\
-					"<!DOCTYPE html>"\
-					"<html>"\
-						"<head>"\
-							"<style>"\
-								"body {"\
-									"overflow: auto;"\
-									"font-family:Sans-serif;"\
-									"font-size: 18px;"\
-									"margin-left: 20px;"\
-								"}"\
-							"</style>"\
-						"</head>"\
-						"<body>"\
-							"<p>"\
-							"OpenFlow Page<br>Placedholder text."\
-							"</p>"\
-						"</body>"\
-					"</html>"\
-							) < SHARED_BUFFER_LEN)
+				if(interfaceCreate_OpenFlow())
 				{
-					TRACE("http.c: html written to buffer");
 					http_send(&shared_buffer, pcb);
-					TRACE("http.c: HTTP sent successfully - %d bytes", strlen(shared_buffer));
+					TRACE("http.c: Page sent successfully - %d bytes", strlen(shared_buffer));
 				}
 				else
 				{
-					TRACE("http.c: WARNING: html truncated to prevent buffer overflow");
+					TRACE("http.c: Unable to serve page - buffer at %d bytes", strlen(shared_buffer));
 				}
 			}
             else if(strcmp(http_msg,"about.htm") == 0)
 			{
-				// Send body
-				if( snprintf(shared_buffer, SHARED_BUFFER_LEN,\
-					"<!DOCTYPE html>"\
-					"<html>"\
-						"<head>"\
-							"<style>"\
-								"body {"\
-									"overflow: auto;"\
-									"font-family:Sans-serif;"\
-									"font-size: 18px;"\
-									"margin-left: 20px;"\
-								"}"\
-							"</style>"\
-						"</head>"\
-						"<body>"\
-							"<p>"\
-							"About Page<br>Placedholder text."\
-							"</p>"\
-						"</body>"\
-					"</html>"\
-							) < SHARED_BUFFER_LEN)
+				if(interfaceCreate_About())
 				{
-					TRACE("http.c: html written to buffer");
 					http_send(&shared_buffer, pcb);
-					TRACE("http.c: HTTP sent successfully - %d bytes", strlen(shared_buffer));
+					TRACE("http.c: Page sent successfully - %d bytes", strlen(shared_buffer));
 				}
 				else
 				{
-					TRACE("http.c: WARNING: html truncated to prevent buffer overflow");
-					TRACE("http.c: no page served");
+					TRACE("http.c: Unable to serve page - buffer at %d bytes", strlen(shared_buffer));
 				}
 			}
 			else
@@ -443,4 +263,317 @@ void http_send(char *buffer, struct tcp_pcb *pcb)
 	if (err == ERR_OK) tcp_output(pcb);
 	tcp_close(pcb);
 	return;
+}
+
+/*
+*	Create and format HTTP/HTML for frames
+*
+*/
+uint8_t interfaceCreate_Frames(void)
+{
+	// Format HTTP response
+	sprintf(shared_buffer,"HTTP/1.1 200 OK\r\n");
+	strcat(shared_buffer,"Connection: close\r\n");
+	strcat(shared_buffer,"Content-Type: text/html; charset=UTF-8\r\n\r\n");
+	// Send frames
+	strcat(shared_buffer, \
+			"<!DOCTYPE html>"\
+			"<html>"\
+				"<head>"\
+					"<title>Zodiac FX</title>"\
+				"</head>"\
+				"<frameset rows=\"75,*\">"\
+					"<frame src=\"header.htm\" name=\"titleframe\" noresize scrolling=\"no\" frameborder=\"1\">"\
+					"<frameset cols=\"160, *\">"\
+						"<frameset rows=\"*,0\">"\
+						"<frame src=\"menu.htm\" name=\"sidebar\" noresize scrolling=\"no\" frameborder=\"1\">"\
+						"</frameset>"\
+						"<frame src=\"home.htm\" name=\"page\" scrolling=\"auto\" frameborder=\"1\">"\
+					"</frameset>"\
+					"<noframes>"\
+						"<body>"\
+						"<p>Browser version not supported."\
+						"</body>"\
+					"</noframes>"\
+				"</frameset>"\
+			"</html>"\
+				);
+	TRACE("http.c: html written to buffer");
+
+	if(strlen(shared_buffer) < 2048)
+	{
+		TRACE("http.c: http/html written to buffer");
+		return 1;
+	}
+	else
+	{
+		TRACE("http.c: WARNING: html truncated to prevent buffer overflow");
+		return 0;
+	}
+}
+
+/*
+*	Create and format HTML for header
+*
+*/
+uint8_t interfaceCreate_Header(void)
+{
+	int hr = (totaltime/2)/3600;
+	int t = (totaltime/2)%3600;
+	int min = t/60;
+
+	// Send header
+	if( snprintf(shared_buffer, SHARED_BUFFER_LEN,\
+			"<!DOCTYPE html>"\
+			"<META http-equiv=\"refresh\" content=\"61\">"\
+			"<html>"\
+				"<head>"\
+				"<style>"\
+					"header {"\
+						"font-family:Sans-serif;"\
+						"position: absolute;"\
+						"top: 0;"\
+						"left: 0;"\
+						"width: 100%%;"\
+						"height: 100%%;"\
+						"overflow: hidden;"\
+						"color: white;"\
+						"background: black;"\
+					"}"\
+					"h1 {"\
+						"margin-top:20px;"\
+						"padding-left: 20px;"\
+					"}"\
+                	"p {"\
+						"font-family:Sans-serif;"\
+						"color: white;"\
+						"position: fixed;"\
+						"right: 150px;"\
+						"top: -5px;"\
+					"}"\
+				"</style>"\
+				"</head>"\
+				"<body>"\
+					"<header>"\
+						"<h1>Zodiac FX</h1>"\
+					"</header>"\
+                	"<p>"\
+						"Firmware Version: %s<br>"\
+						"CPU Temp: %d C<br>"\
+						"Uptime: %02d:%02d"\
+					"</p>"\
+				"</body>"\
+			"</html>"\
+				, VERSION, (int)ul_temp, hr, min) < SHARED_BUFFER_LEN)
+	{
+		TRACE("http.c: html written to buffer");
+		return 1;
+	}
+	else
+	{
+		TRACE("http.c: WARNING: html truncated to prevent buffer overflow");
+		return 0;
+	}
+}
+
+/*
+*	Create and format HTML for menu page
+*
+*/
+uint8_t interfaceCreate_Menu(void)
+{
+	// Send menu
+	if( snprintf(shared_buffer, SHARED_BUFFER_LEN,\
+		"<!DOCTYPE html>"\
+		"<html>"\
+			"<head>"\
+				"<style>"\
+				"body {"\
+					"font-family:Sans-serif;"\
+					"line-height: 1.9em;"\
+					"font-size: 20px;"\
+					"font-weight: bold;"\
+					"background: #F6F6F6;"\
+				"}"\
+				"body ul {"\
+					"list-style-type: none;"\
+					"margin: 10px;"\
+					"padding: 0;"\
+				"}"\
+				"body ul a {"\
+					"color: black;"\
+					"text-decoration: none;"\
+				"}"\
+				"body ul a:active {"\
+					"font-weight: 700;"\
+				"}"\
+				"</style>"\
+			"</head>"\
+				"<body>"\
+					"<ul>"\
+						"<li><a href=\"home.htm\" target=\"page\">Home</a></li>"\
+						"<li><a href=\"config.htm\" target=\"page\">Config</a></li>"\
+						"<li><a href=\"openflow.htm\" target=\"page\">OpenFlow</a></li>"\
+						"<li><a href=\"about.htm\" target=\"page\">About</a></li>"\
+					"</ul>"\
+				"</body>"\
+		"</html>"\
+				) < SHARED_BUFFER_LEN)
+	{
+		TRACE("http.c: html written to buffer");
+		return 1;
+	}
+	else
+	{
+		TRACE("http.c: WARNING: html truncated to prevent buffer overflow");
+		return 0;
+	}
+}
+
+/*
+*	Create and format HTML for home page
+*
+*/
+uint8_t interfaceCreate_Home(void)
+{
+	if( snprintf(shared_buffer, SHARED_BUFFER_LEN,\
+		"<!DOCTYPE html>"\
+		"<html>"\
+			"<head>"\
+				"<style>"\
+					"body {"\
+						"overflow: auto;"\
+						"font-family:Sans-serif;"\
+						"font-size: 18px;"\
+						"margin-left: 20px;"\
+					"}"\
+				"</style>"\
+			"</head>"\
+			"<body>"\
+				"<p>"\
+				"Home Page<br>Placedholder text."\
+				"</p>"\
+			"</body>"\
+		"</html>"\
+				) < SHARED_BUFFER_LEN)
+	{
+		TRACE("http.c: html written to buffer");
+		return 1;
+	}
+	else
+	{
+		TRACE("http.c: WARNING: html truncated to prevent buffer overflow");
+		return 0;
+	}
+}
+
+/*
+*	Create and format HTML for config page
+*
+*/
+uint8_t interfaceCreate_Config(void)
+{
+	if( snprintf(shared_buffer, SHARED_BUFFER_LEN,""\
+		"<!DOCTYPE html>"\
+		"<html>"\
+			"<head>"\
+				"<style>"\
+					"body {"\
+						"overflow: auto;"\
+						"font-family:Sans-serif;"\
+						"font-size: 18px;"\
+						"margin-left: 20px;"\
+					"}"\
+				"</style>"\
+			"</head>"\
+			"<body>"\
+				"<p>"\
+				"Config Page<br>Placedholder text."\
+				"</p>"\
+			"</body>"\
+		"</html>"\
+				) < SHARED_BUFFER_LEN)
+	{
+		TRACE("http.c: html written to buffer");
+		return 1;
+	}
+	else
+	{
+		TRACE("http.c: WARNING: html truncated to prevent buffer overflow");
+		return 0;
+	}
+}
+
+/*
+*	Create and format HTML for openflow page
+*
+*/
+uint8_t interfaceCreate_OpenFlow(void)
+{
+	if( snprintf(shared_buffer, SHARED_BUFFER_LEN,\
+	"<!DOCTYPE html>"\
+	"<html>"\
+		"<head>"\
+			"<style>"\
+				"body {"\
+					"overflow: auto;"\
+					"font-family:Sans-serif;"\
+					"font-size: 18px;"\
+					"margin-left: 20px;"\
+				"}"\
+			"</style>"\
+		"</head>"\
+		"<body>"\
+			"<p>"\
+			"OpenFlow Page<br>Placedholder text."\
+			"</p>"\
+		"</body>"\
+	"</html>"\
+			) < SHARED_BUFFER_LEN)
+	{
+		TRACE("http.c: html written to buffer");
+		return 1;
+	}
+	else
+	{
+		TRACE("http.c: WARNING: html truncated to prevent buffer overflow");
+		return 0;
+	}
+}
+
+/*
+*	Create and format HTML for about page
+*
+*/
+uint8_t interfaceCreate_About(void)
+{
+	if( snprintf(shared_buffer, SHARED_BUFFER_LEN,\
+		"<!DOCTYPE html>"\
+		"<html>"\
+			"<head>"\
+				"<style>"\
+					"body {"\
+						"overflow: auto;"\
+						"font-family:Sans-serif;"\
+						"font-size: 18px;"\
+						"margin-left: 20px;"\
+					"}"\
+				"</style>"\
+			"</head>"\
+			"<body>"\
+				"<p>"\
+				"About Page<br>Placedholder text."\
+				"</p>"\
+			"</body>"\
+		"</html>"\
+				) < SHARED_BUFFER_LEN)
+	{
+		TRACE("http.c: html written to buffer");
+		return 1;
+	}
+	else
+	{
+		TRACE("http.c: WARNING: html truncated to prevent buffer overflow");
+		return 0;
+	}
 }
