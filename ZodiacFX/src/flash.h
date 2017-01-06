c3e709e40c86b3436a93f4295504412c89b52aaf
Paul Zanna <paul@northboundnetworks.com>
2017-01-06 20:44:33 +1100

    v0.72 final save

--- ZodiacFX/src/flash.h
+++ ZodiacFX/src/flash.h
@@ -8,7 +8,7 @@
 
 /*
  * This file is part of the Zodiac FX firmware.
- * Copyright (c) 2016 Northbound Networks.
+ * Copyright (c) 2016 Google Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
