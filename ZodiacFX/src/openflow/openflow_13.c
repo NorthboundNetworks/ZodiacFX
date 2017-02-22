7e12247467a3dc4482bfc768495b6ee409a75475
Kristopher Chen <kristopherchen.kc@gmail.com>
2017-02-22 18:06:51 +1100

    Add reply all for meter configuration

--- ZodiacFX/src/openflow/openflow_13.c
+++ ZodiacFX/src/openflow/openflow_13.c
@@ -1312,10 +1312,73 @@ int multi_meter_config_reply13(uint8_t *buffer, struct ofp13_multipart_request *
 	{
 		TRACE("openflow_13.c: request for all meter configurations");
 
-		// Reply with all meter configurations
-		/* ***** TODO ***** */
+		/* Reply with all meter configurations */
 		
-		//return (value);
+		// Count the number of meters configured, and the total number of bands
+		int meter_index = 0;
+		uint16_t bands_counter = 0;
+		while(meter_entry[meter_index] != NULL && meter_index < MAX_METER_13)
+		{
+			bands_counter += meter_entry[meter_index]->band_count;
+			meter_index++;
+		};
+		
+		TRACE("openflow_13.c: %d meters in meter table, %d bands", meter_index, (int)bands_counter);
+		
+		// Calculate total size - replysize + (number of meters)*configsize + (total number of bands)*bandsize
+		uint16_t	total_size = sizeof(struct ofp13_multipart_reply) + (meter_index*sizeof(struct ofp13_meter_config)) + (bands_counter*sizeof(struct ofp13_meter_band_drop));
+		
+		// Format reply
+		reply.type				= htons(OFPMP13_METER_CONFIG);
+		reply.flags				= 0;	// Single reply
+	
+		// Format header
+		reply.header.version	= OF_Version;
+		reply.header.type		= OFPT13_MULTIPART_REPLY;
+		reply.header.length		= htons(total_size);
+		reply.header.xid		= req->header.xid;
+	
+		// Copy reply
+		memcpy(buffer_ptr, &reply, sizeof(struct ofp13_multipart_reply));
+		buffer_ptr += sizeof(struct ofp13_multipart_reply);
+
+		meter_index = 0;
+		// Loop & format each meter stats reply
+		while(meter_entry[meter_index] != NULL && meter_index < MAX_METER_13)
+		{
+			// Format reply with specified meter configuration
+			meter_config.length		= htons(sizeof(struct ofp13_meter_config) + (meter_entry[meter_index]->band_count*sizeof(struct ofp13_meter_band_drop)));
+			meter_config.flags		= htons(meter_entry[meter_index]->flags);
+			meter_config.meter_id	= htonl(meter_entry[meter_index]->meter_id);
+			
+			// Copy configuration
+			memcpy(buffer_ptr, &meter_config, sizeof(struct ofp13_meter_config));
+			buffer_ptr += sizeof(struct ofp13_meter_config);
+			
+			// Format bands
+			int bands_processed = 0;
+			struct ofp13_meter_band_drop * ptr_band;
+			ptr_band = &(meter_entry[meter_index]->bands);
+			struct ofp13_meter_band_drop * ptr_buffer_band;
+			ptr_buffer_band = buffer_ptr;
+			
+			while(bands_processed < meter_entry[meter_index]->band_count)
+			{
+				ptr_buffer_band->type		= htons(ptr_band->type);
+				ptr_buffer_band->len		= htons(sizeof(struct ofp13_meter_band_drop));
+				ptr_buffer_band->rate		= htonl(ptr_band->rate);
+				ptr_buffer_band->burst_size	= htonl(ptr_band->burst_size);
+				
+				ptr_buffer_band++;
+				ptr_band++;	// Move to next band
+				bands_processed++;
+			}
+			
+			// update buffer pointer
+			buffer_ptr = ptr_buffer_band;
+		}
+		
+		return (buffer_ptr - buffer);	// return length
 	}
 	
 	TRACE("openflow_13.c: request for meter configuration (meter id %d)", req_id);
