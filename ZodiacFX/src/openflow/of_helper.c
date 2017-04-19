fcebcb03f6963e0962193c34bbc8523cebf08823
Kristopher Chen <kristopherchen.kc@gmail.com>
2017-04-19 16:14:42 +1000

    Modify port status message output count
    Support 8 ports

--- ZodiacFX/src/openflow/of_helper.c
+++ ZodiacFX/src/openflow/of_helper.c
@@ -64,6 +64,7 @@ extern struct ofp13_flow_mod *flow_match13[MAX_FLOWS_13];
 extern uint8_t *ofp13_oxm_match[MAX_FLOWS_13];
 extern uint8_t *ofp13_oxm_inst[MAX_FLOWS_13];
 extern uint16_t ofp13_oxm_inst_size[MAX_FLOWS_13];
+extern uint8_t total_ports;
 
 // Local Variables
 uint8_t timer_alt;
@@ -140,7 +141,7 @@ void nnOF_timer(void)
 	} else if (timer_alt == 1){
 		update_port_status();
 		// If port status has changed send a port status message
-		for (int x=0;x<4;x++)
+		for (int x=0;x<total_ports;x++)
 		{
 			if (last_port_status[x] != port_status[x] && OF_Version == 1 && Zodiac_Config.of_port[x] == 1) port_status_message10(x);
 			if (last_port_status[x] != port_status[x] && OF_Version == 4 && Zodiac_Config.of_port[x] == 1) port_status_message13(x);
